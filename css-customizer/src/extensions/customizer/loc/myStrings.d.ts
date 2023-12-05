8073f6ff76225687755596b7d714c891e43948bf
GeoCyber <allauddinshamsul@hotmail.com>
2023-12-05 09:27:43 +0800

    add

---
+++ css-customizer/src/extensions/customizer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICustomizerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CustomizerApplicationCustomizerStrings' {
+  const strings: ICustomizerApplicationCustomizerStrings;
+  export = strings;
+}
